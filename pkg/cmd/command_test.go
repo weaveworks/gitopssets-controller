4f8daadcea79cc42359eec81e9e0e7e436012764
Kevin McDermott <kevin@weave.works>
2023-07-06 15:26:57 +0100

    Support multidoc when rendering the CLI
    When rendering a template from the CLI, all GitOpsSets in the YAML are
    rendered, splitting on the YAML multi-doc indicator.
    
    This allows processing multiple GitOpSets in the same file.

--- pkg/cmd/command_test.go
+++ pkg/cmd/command_test.go
@@ -40,3 +40,55 @@ spec:
 		t.Fatalf("failed to generate:\n%s", diff)
 	}
 }
+
+func TestRenderGitOpsSet_with_multiple_sets(t *testing.T) {
+	var out strings.Builder
+
+	err := renderGitOpsSet("testdata/list_sets.yaml", setup.DefaultGenerators, true, &out)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := `---
+apiVersion: kustomize.toolkit.fluxcd.io/v1beta2
+kind: Kustomization
+metadata:
+  labels:
+    app.kubernetes.io/instance: dev
+    app.kubernetes.io/name: go-demo
+    com.example/team: dev-team
+    templates.weave.works/name: gitopsset-sample
+    templates.weave.works/namespace: ""
+  name: dev-demo
+  namespace: default
+spec:
+  interval: 5m
+  path: ./examples/kustomize/environments/dev
+  prune: true
+  sourceRef:
+    kind: GitRepository
+    name: go-demo-repo
+---
+apiVersion: kustomize.toolkit.fluxcd.io/v1beta2
+kind: Kustomization
+metadata:
+  labels:
+    app.kubernetes.io/instance: dev
+    app.kubernetes.io/name: go-demo
+    com.example/team: dev-team
+    templates.weave.works/name: second-gitopsset-sample
+    templates.weave.works/namespace: ""
+  name: dev-demo-2
+  namespace: default
+spec:
+  interval: 5m
+  path: ./examples/kustomize/environments/dev
+  prune: true
+  sourceRef:
+    kind: GitRepository
+    name: go-demo-repo
+`
+	if diff := cmp.Diff(want, out.String()); diff != "" {
+		t.Fatalf("failed to generate:\n%s", diff)
+	}
+}
